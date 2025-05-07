fa5bee0e142ba23db1f51dee583c9a52db77e9eb
박재용 <jy.park@pia.space>
2025-05-08 02:07:22 +0900

    feat: optimizer strip 및 모델 업데이트 시 transforms 제거와 원본 같이 저장

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -571,6 +571,33 @@ def strip_optimizer(f: Union[str, Path] = "best.pt", s: str = "", updates: dict 
 
     # Update model
     if x.get("ema"):
+        # del x["ema"].transforms
+        x["model"] = x["ema"]  # replace model with EMA
+    if hasattr(x["model"], "args"):
+        x["model"].args = dict(x["model"].args)  # convert from IterableSimpleNamespace to dict
+    if hasattr(x["model"], "criterion"):
+        x["model"].criterion = None  # strip loss criterion
+    x["model"].half()  # to FP16
+    for p in x["model"].parameters():
+        p.requires_grad = False
+
+    # Update other keys
+    args = {**DEFAULT_CFG_DICT, **x.get("train_args", {})}  # combine args
+    for k in "optimizer", "best_fitness", "ema", "updates":  # keys
+        x[k] = None
+    x["epoch"] = -1
+    x["train_args"] = {k: v for k, v in args.items() if k in DEFAULT_CFG_KEYS}  # strip non-default keys
+    # x['model'].args = x['train_args']
+
+    # Save
+    combined = {**metadata, **x, **(updates or {})}
+    torch.save(combined, s or f)  # combine dicts (prefer to the right)
+    mb = os.path.getsize(s or f) / 1e6  # file size
+    LOGGER.info(f"Optimizer stripped from {f},{f' saved as {s},' if s else ''} {mb:.1f}MB")
+    
+    # Update model and no transform
+    f = "best_no_transform.pt"
+    if x.get("ema"):
         del x["ema"].transforms
         x["model"] = x["ema"]  # replace model with EMA
     if hasattr(x["model"], "args"):
